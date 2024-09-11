be73c10c4b0936243717c5a5076a4d5b1eb14309
gravity34031 <gravity2507@gmail.com>
2024-09-11 19:54:21 +0500

    Task1

---
+++ Tyuiu.KomarovNA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+namespace Tyuiu.KomarovNA.Sprint0.Task1.V0
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string name = "Никита";
+            int age = 21;
+            Console.WriteLine("здравствуйте меня зовут " + name + " мне " + age + " год.");
+            Console.ReadLine();
+        }
+    }
+}
